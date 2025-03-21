4e111e75bb24f09632ff706762fe44b025aa4e4d
tengyunfeng <tengyunfeng@gongguizhijia.com>
2025-03-21 09:24:08 +0800

    add joinPath

--- UtilsPath.mjs
+++ UtilsPath.mjs
@@ -1,6 +1,11 @@
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import { join, resolve } from "node:path";
 
 export function getDirname(metaUrl) {
   return dirname(fileURLToPath(metaUrl));
 }
+
+export const joinPath = (...args) => {
+  return resolve(join(...args));
+};
